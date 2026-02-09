7f9103194abfd5c04c2f0c3454ea92d01ff48ac3
Kelim Selmanović <kelim.selmanovic@stu.ibu.edu.ba>
2026-02-09 19:01:14 +0100

    Promena

--- src/main/java/com/sandzaksell/sandzaksell/config/JwtFilter.java
+++ src/main/java/com/sandzaksell/sandzaksell/config/JwtFilter.java
@@ -29,6 +29,14 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setHeader("Access-Control-Allow-Origin", "https://sandzak-sell-marketplace.vercel.app");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Origin, Accept");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
 
         String authHeader = request.getHeader("Authorization");
         String token = null;
