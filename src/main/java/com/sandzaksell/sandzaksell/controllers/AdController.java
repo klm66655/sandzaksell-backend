e2b181deef93043026b4c19f9f4d099cf7ae131f
Kelim Selmanović <kelim.selmanovic@stu.ibu.edu.ba>
2026-04-04 03:18:55 +0200

    proba

--- src/main/java/com/sandzaksell/sandzaksell/controllers/AdController.java
+++ src/main/java/com/sandzaksell/sandzaksell/controllers/AdController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/ads")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5173", "https://sandzak-sell-marketplace.vercel.app"})
+
 public class AdController {
 
     private final AdService adService;
